34842013693ab3062f755c88a1ddf656766d5687
Harshith <harshith.jethu@gmail.com>
2022-07-06 02:24:17 +0530

    Made the databse

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class VideoDetails(models.Model):
+    video_id = models.CharField(max_length = 100)
+    title = models.CharField(max_length = 100)
+    description = models.CharField(max_length = 100)
+    published_time = models.CharField(max_length = 100)
+    thumbnail_url_default = models.CharField(max_length = 100)
+    thumbnail_url_medium = models.CharField(max_length = 100)
+    thumbnail_url_high = models.CharField(max_length = 100)
+
+    def __str__(self):
+        return str(self.published_time)
+    class Meta:
+       ordering = ('-published_time',)
